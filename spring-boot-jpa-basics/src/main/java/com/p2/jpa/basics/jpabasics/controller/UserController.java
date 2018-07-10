35b40ba31dd521bfee068f8dadf3451d1a936209
pintub <pintupnt542@gmail.com>
2018-07-10 21:59:11 +0530

    HATEOAS example

--- spring-boot-jpa-basics/src/main/java/com/p2/jpa/basics/jpabasics/controller/UserController.java
+++ spring-boot-jpa-basics/src/main/java/com/p2/jpa/basics/jpabasics/controller/UserController.java
@@ -3,16 +3,20 @@ package com.p2.jpa.basics.jpabasics.controller;
 import com.p2.jpa.basics.jpabasics.entity.User;
 import com.p2.jpa.basics.jpabasics.service.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.persistence.Id;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @RestController
 public class UserController {
@@ -21,25 +25,34 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("/users")
-    public List<User> getUsers(){
+    public List<User> getUsers() {
         return userRepository.findAll();
     }
 
     @GetMapping("/user/{id}")
-    public User getUser(@PathVariable Long id){
-         Optional<User> user = userRepository.findById(id);
-         return user.get();
+    public Resource<User> getUser(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+
+        //Add Link
+        //"all-users", SERVER_PATH + "/users"
+        //retrieveAllUsers
+        Resource<User> resource = new Resource<User>(user.get());
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).getUsers());
+
+        resource.add(linkTo.withRel("all-users"));
+
+        return resource;
     }
 
     @PostMapping("/userV2")
-    public User getUserV2(@RequestBody User user){
+    public User getUserV2(@RequestBody User user) {
         Optional<User> userById = userRepository.findById(user.getId());
 
         User userFound = null;
 
         try {
             userFound = userById.get();
-        } catch (NoSuchElementException e){
+        } catch (NoSuchElementException e) {
             throw new UnsupportedOperationException("User " + user.getId() + " Not found ..");
         }
 
@@ -48,8 +61,8 @@ public class UserController {
 
     //Best practice to return URI of newly created resource for POST method
     @PostMapping("/createUser")
-    public ResponseEntity createUser(@Valid @RequestBody User user){
-        User newUser =  userRepository.save(user);
+    public ResponseEntity createUser(@Valid @RequestBody User user) {
+        User newUser = userRepository.save(user);
 
         URI location = ServletUriComponentsBuilder.fromCurrentContextPath().path("/user")
                 .path("/{id}").buildAndExpand(newUser.getId()).toUri();
